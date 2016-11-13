8aa74d5901a7563440cba9fe2119a1077dc3a98a
Christian Harper-Cyr <charpercyr@gmail.com>
2016-11-12 22:49:45 -0500

    Cree le code de base + libs

--- vulkan/main.cpp
+++ vulkan/main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
+#include <vulkan/vulkan.h>
+#include <GLFW/glfw3.h>
 
 using namespace std;
 
 int main()
 {
+	glfwInit();
+
+	uint32_t count;
+	vkEnumerateInstanceExtensionProperties(nullptr, &count, nullptr);
+
+	cout << count << " extensions" << endl;
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan", nullptr, nullptr);
+
+	while (!glfwWindowShouldClose(window))
+		glfwPollEvents();
+
+	glfwDestroyWindow(window);
+
+	glfwTerminate();
 	return 0;
 }
